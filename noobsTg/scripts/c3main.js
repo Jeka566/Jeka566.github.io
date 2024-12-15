53e7ccefd742010aeecd8f03812958a2c6899fcd
Jeka566 <jeka2016@gmail.com>
2024-12-15 22:41:58 +0300

    Add files via upload

---
+++ noobsTg/scripts/c3main.js
@@ -0,0 +1,6 @@
+import "./../lodash.custom.min.js";
+import "./../tween.js";
+import "./c3runtime.js";
+import "./objRefTable.js";
+import "./project/Confetti.js";
+import "./project/scriptsInEvents.js";
