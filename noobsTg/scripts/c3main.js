0eedd39e6951b2e8e6b1fd6b30a07bffb3e3b7fc
Jeka566 <jeka2016@gmail.com>
2024-12-26 13:29:46 +0300

    Delete noobsTg directory

--- noobsTg/scripts/c3main.js
+++
@@ -1,6 +0,0 @@
-import "./../lodash.custom.min.js";
-import "./../tween.js";
-import "./c3runtime.js";
-import "./objRefTable.js";
-import "./project/Confetti.js";
-import "./project/scriptsInEvents.js";
