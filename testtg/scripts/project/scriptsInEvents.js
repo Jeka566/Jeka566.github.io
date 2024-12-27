1df4a39722ec6d2dad2065f8d3ddf00b18cad230
Jeka566 <jeka2016@gmail.com>
2024-12-27 19:41:16 +0300

    f

--- testtg/scripts/project/scriptsInEvents.js
+++ testtg/scripts/project/scriptsInEvents.js
@@ -1,25 +1,16 @@
 
 
 
-const scriptsInEvents = {
+<!DOCTYPE html> 
 
-	async списоксобытий1_Event1_Act1(runtime, localVars)
-	{
-		const script = document.createElement('script');
-		script.type = 'text/javascript';
-		script.defer = true;
-		script.src = 'https://s3.programmatica.com/scripts/ntvk/latest/main.bundle.js';
-		
-		script.onload = () => {
-		    const ntvkTag = document.createElement('ntvk-tag');
-		    ntvkTag.setAttribute('key', 'ctlu0k2ntenc73fm92g0#ctlu3j2ntenc73fm92gg');
-		    document.body.appendChild(ntvkTag);
-		};
-		
-		document.head.appendChild(script);
-	}
+<html>
+<body>
+<script defer type="text/javascript"
+src="https:/s3.programmatica.com/scripts/ntvk/lates/main.bundle.js">
 
-};
+<ntvk-tag> 
+key="ctlu0k2ntenc73fm92g0#ctlu3j2ntenc73fm92gg"
+<>/ntvk-tag>
+</body>
 
-self.C3.ScriptsInEvents = scriptsInEvents;
-
+</html>
