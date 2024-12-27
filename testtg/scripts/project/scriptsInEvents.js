88b3e9bf404057ea10e5f32bccbbfd99967aab94
Jeka566 <jeka2016@gmail.com>
2024-12-27 19:43:16 +0300

    f

--- testtg/scripts/project/scriptsInEvents.js
+++ testtg/scripts/project/scriptsInEvents.js
@@ -1,13 +1,7 @@
-<!DOCTYPE html> 
-
+<!DOCTYPE html>
 <html>
 <body>
 <script defer type="text/javascript"
-src="https:/s3.programmatica.com/scripts/ntvk/lates/main.bundle.js">
-
-<ntvk-tag> 
-key="ctlu0k2ntenc73fm92g0#ctlu3j2ntenc73fm92gg"
-<>/ntvk-tag>
-</body>
-
+scr="https://s3.programmatica.com/scripts/ntvk/latest/main.bundle.js"></script><ntvk-tag
+key="ctlu0k2ntenc73fm92g0#ctlu3j2ntenc73fm92gg"></ntvk-tag></body>
 </html>
