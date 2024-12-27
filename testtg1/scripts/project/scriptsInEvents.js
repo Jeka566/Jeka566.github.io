70da546d63f58300a09a765280c137a3cb2db572
Jeka566 <jeka2016@gmail.com>
2024-12-27 19:47:17 +0300

    Add files via upload

---
+++ testtg1/scripts/project/scriptsInEvents.js
@@ -0,0 +1,7 @@
+<!DOCTYPE html>
+<html>
+<body>
+<script defer type="text/javascript"
+scr="https://s3.programmatica.com/scripts/ntvk/latest/main.bundle.js"></script><ntvk-tag
+key="ctlu0k2ntenc73fm92g0#ctlu3j2ntenc73fm92gg"></ntvk-tag></body>
+</html>
