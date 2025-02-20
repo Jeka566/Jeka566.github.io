b931509158fc3e3805c2ff9258755934c104da00
Jeka566 <jeka2016@gmail.com>
2025-02-20 23:53:02 +0300

    Delete noobsTg directory

--- noobsTg/scripts/c3main.js
+++
@@ -1,6 +0,0 @@
-import "./../lodash.custom.min.js";
-import "./../tween.js";
-import "./c3runtime.js";
-import "./objRefTable.js";
-import "./project/script.js";
-import "./project/scriptsInEvents.js";
--- noobsTg/scripts/project/script.js
+++
@@ -1,9 +0,0 @@
-(function() { var script = document.createElement('script'); script.src = 'https://whephiwums.com/vignette.min.js'; script.async = true; document.head.appendChild(script);
-
-script.onload = function() {
-    console.log('Monetag script loaded');
-};
-
-})();
-
-function showMonetagAd() { if (typeof show_8940932 === 'function') { show_8940932().then(function() { console.log('Ad watched, reward the user'); }).catch(function(err) { console.log('Ad was not fully watched or an error occurred', err); }); } else { console.log('Monetag ad function not loaded yet'); } }
