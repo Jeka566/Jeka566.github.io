f20bdc68eda4e7ebd3bd4930a45d7c105eb72c62
Jeka566 <jeka2016@gmail.com>
2025-04-10 00:41:07 +0300

    Add files via upload

---
+++ catcoins/scripts/objRefTable.js
@@ -0,0 +1,49 @@
+const C3 = self.C3;
+self.C3_GetObjectRefTable = function () {
+	return [
+		C3.Plugins.Browser,
+		C3.Plugins.Eponesh_GameScore,
+		C3.Plugins.Sprite,
+		C3.Plugins.Touch,
+		C3.Plugins.GamePush_Channels,
+		C3.Plugins.System.Cnds.OnLayoutStart,
+		C3.Plugins.System.Acts.SetVar,
+		C3.Plugins.System.Exps.int,
+		C3.Plugins.System.Exps.tokenat,
+		C3.Plugins.Browser.Exps.URL,
+		C3.Plugins.Browser.Acts.ConsoleLog,
+		C3.Plugins.System.Cnds.EvaluateExpression,
+		C3.Plugins.System.Acts.Wait,
+		C3.Plugins.GamePush_Channels.Acts.SendFeedMessage,
+		C3.Plugins.Touch.Cnds.OnTapGestureObject,
+		C3.Plugins.Eponesh_GameScore.Acts.SocialsShare,
+		C3.Plugins.Eponesh_GameScore.Exps.PlayerID,
+		C3.Plugins.System.Cnds.Every,
+		C3.Plugins.GamePush_Channels.Acts.FetchFeedMessages,
+		C3.Plugins.GamePush_Channels.Cnds.OnFeedMessagesFetch,
+		C3.Plugins.GamePush_Channels.Cnds.EachMessage,
+		C3.Plugins.System.Acts.AddVar,
+		C3.Plugins.Eponesh_GameScore.Acts.PlayerAdd,
+		C3.Plugins.GamePush_Channels.Exps.CurMessageText,
+		C3.Plugins.GamePush_Channels.Acts.DeleteMessage,
+		C3.Plugins.GamePush_Channels.Exps.CurMessageID,
+		C3.Plugins.Eponesh_GameScore.Acts.PlayerSync
+	];
+};
+self.C3_JsPropNameTable = [
+	{Browser: 0},
+	{GamePush: 0},
+	{Спрайт: 0},
+	{Тач: 0},
+	{GamePushChannels: 0},
+	{Refid: 0},
+	{MessageCount: 0}
+];
+
+self.InstanceType = {
+	Browser: class extends self.IInstance {},
+	GamePush: class extends self.IInstance {},
+	Спрайт: class extends self.ISpriteInstance {},
+	Тач: class extends self.IInstance {},
+	GamePushChannels: class extends C3.Plugins.GamePush_Channels.Instance {}
+}
