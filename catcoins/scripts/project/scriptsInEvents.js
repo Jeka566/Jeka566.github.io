5b99f337ab312c9e65bfea89fb23fff28ba1e0ab
Jeka566 <jeka2016@gmail.com>
2025-04-11 16:29:05 +0300

    Add files via upload

---
+++ catcoins/scripts/project/scriptsInEvents.js
@@ -0,0 +1,23 @@
+
+
+const scriptsInEvents = {
+
+	async EventSheet1_Event1_Act2(runtime, localVars)
+	{
+		try {
+		    const ref1 = window.Telegram?.WebApp?.initDataUnsafe?.start_param || "";
+		    const params = new URLSearchParams(window.location.search);
+		    const ref2 = params.get("tgWebAppStartParam") || params.get("startapp") || "";
+		    const match = decodeURIComponent(location.hash).match(/start_param=([^&]+)/);
+		    const ref3 = match ? match[1] : "";
+		
+		    const ref = ref1 || ref2 || ref3 || "";
+		    runtime.globalVars.Idref = ref;
+		} catch (e) {
+		    console.warn("Ошибка при получении Idref:", e.message);
+		}
+		
+	}
+};
+
+self.C3.ScriptsInEvents = scriptsInEvents;
