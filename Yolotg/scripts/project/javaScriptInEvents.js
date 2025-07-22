5cddcea5bef22db0dae63bfa8dded5d7341387bd
Jeka566 <jeka2016@gmail.com>
2025-07-22 12:15:39 +0300

    Add files via upload

---
+++ Yolotg/scripts/project/javaScriptInEvents.js
@@ -0,0 +1,16 @@
+
+
+const scriptsInEvents = {
+
+	async Evmenu_Event3_Act1(runtime, localVars)
+	{
+		p_adextra()
+	},
+
+	async Evmenu_Event7_Act1(runtime, localVars)
+	{
+		p_adextra()
+	}
+};
+
+globalThis.C3.JavaScriptInEvents = scriptsInEvents;
