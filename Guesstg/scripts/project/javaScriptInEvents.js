dce9a3f1d075d85460fb49b90d2e5ab8445992fa
Jeka566 <jeka2016@gmail.com>
2025-07-22 11:42:50 +0300

    Add files via upload

---
+++ Guesstg/scripts/project/javaScriptInEvents.js
@@ -0,0 +1,16 @@
+
+
+const scriptsInEvents = {
+
+	async Es_menu_Event3_Act1(runtime, localVars)
+	{
+		p_adextra()
+	},
+
+	async Es_menu_Event16_Act1(runtime, localVars)
+	{
+		p_adextra()
+	}
+};
+
+globalThis.C3.JavaScriptInEvents = scriptsInEvents;
